b3d33dc20d209c17ce72f6734792d89eb48fe517
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-03 21:32:50 +0000

    commit

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: cedric
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+cookie = 0
+
+while True:
+    if button_a.is_pressed():
+        cookie = cookie + 1
+        display.show(cookie)
+
+    if button_b.is_pressed():
+        cookie = 0
+        display.show(cookie)
