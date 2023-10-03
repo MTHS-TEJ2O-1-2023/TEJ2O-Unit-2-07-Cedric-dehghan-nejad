a8b9f603f5e380075638e42450959c3fcfd0d133
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-03 13:23:39 -0400

    typescript change 2

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created on: oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let cookie: number
+cookie = 0 
+
+//input
+input.onButtonPressed(Button.A, function() {
+cookie = (cookie +1)
+
+basic.showNumber (cookie)
+
+input.onButtonPressed(Button.B, function() {
+cookie = 0
+basic.showNumber
+})
+})
