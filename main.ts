4af4e1ca9816f90acf66d5ec23bbefec3830b5a9
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-03 16:43:38 -0400

    typescript change 3

--- main.ts
+++ main.ts
@@ -9,16 +9,16 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 let cookie: number
-cookie = 0 
-
-//input
-input.onButtonPressed(Button.A, function() {
-cookie = (cookie +1)
-
-basic.showNumber (cookie)
-
-input.onButtonPressed(Button.B, function() {
 cookie = 0
-basic.showNumber
-})
+
+// input
+input.onButtonPressed(Button.A,function() {
+  cookie = (cookie + 1)
+
+  basic.showNumber (cookie)
+
+  input.onButtonPressed(Button.B, function() {
+    cookie = 0
+    basic.showNumber (cookie)
+  })
 })
